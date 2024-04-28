1dfc99563cb7b11968b17d2ef8a8abaff2a7f05b
muanziri <munaziribienaime@yahoo.com>
2024-04-28 19:09:35 +0200

    pool

--- usualRoutes/idex.js
+++ usualRoutes/idex.js
@@ -9,7 +9,7 @@ const app = Router();
 const saltRounds = 10;
 
 const checkAuthenticated = (req, res, next) => {
-  console.log(String(req.cookies));
+  console.log(req.cookies);
   if(req.user){
     res.locals.user = req.user;
     next();
