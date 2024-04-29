fcd0e7aa98cd0e98230c212f11830356f9bce862
muanziri <munaziribienaime@yahoo.com>
2024-04-29 08:15:46 +0200

    lkmlk

--- usualRoutes/idex.js
+++ usualRoutes/idex.js
@@ -9,7 +9,7 @@ const app = Router();
 const saltRounds = 10;
 
 const checkAuthenticated = (req, res, next) => {
-  console.log(req.cookies);
+  console.log(req.cookies['121200909']);
   if(req.user){
     res.locals.user = req.user;
     next();
