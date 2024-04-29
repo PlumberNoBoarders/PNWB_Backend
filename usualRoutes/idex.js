a5c8cd308fda36b93d73cf9baa5f696ae9a7b1f7
PlumberNoBoarders <166424621+PlumberNoBoarders@users.noreply.github.com>
2024-04-29 08:36:16 +0200

    Update idex.js

--- usualRoutes/idex.js
+++ usualRoutes/idex.js
@@ -9,7 +9,7 @@ const app = Router();
 const saltRounds = 10;
 
 const checkAuthenticated = (req, res, next) => {
-  console.log(req.cookies['language'],{'hello':'sjjan'})
+  console.log(req.cookies['language'],'hellllllo')
   if(req.user){
     res.locals.user = req.user;
     next();
